d7a0056b50534a288e3c91331098c5f88b6cb78e
Alessandro Degliangioli <alessandro281199@gmail.com>
2020-12-03 10:06:21 +0100

    modifica e aggiunta file

--- js/main.js
+++ js/main.js
@@ -1,6 +1,18 @@
 var el = document.getElementsByClassName('mobile-menu')[0];
 
 // Event listener al click del mouse, che alterna l'hamburger a una X
-el.addEventListener('click', function(){
+el.addEventListener('click', function () {
     el.classList.toggle('change');
+})
+
+
+var mobileNavigation = document.getElementsByClassName('mobile-navigation')[0];
+
+el.addEventListener('click', function () {
+    if (mobileNavigation.style.display == 'block') {
+        mobileNavigation.style.display = 'none';
+    }
+    else {
+        mobileNavigation.style.display = 'block';
+    }
 })
